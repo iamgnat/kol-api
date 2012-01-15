b92dec66a5ce533f99e486230e0c61a37f93e004
iamgnat <iamgnat@1273db1c-fa75-11de-9e29-7d1cd92887ef>
2012-01-15 04:11:15 +0000

    Added use() method

--- lib/perl/KoL/Item/Usable.pm
+++ lib/perl/KoL/Item/Usable.pm
@@ -22,4 +22,50 @@ sub new {
     return($self);
 }
 
+sub use {
+    my $self = shift;
+    my $count = shift || 1;
+    
+    if (ref($self->{'controller'}) ne 'KoL::Inventory') {
+        $@ = "Only inventory items may be used.";
+        return(undef);
+    }
+    
+    if ($count !~ m/^\d+$/) {
+        $@ = "'$count' is not a valid count.";
+        return(undef);
+    }
+    
+    # Use the method rather than a direct check to force an update if needed.
+    if ($count > $self->count()) {
+        $@ = "You may not use more than you have.";
+        return(undef);
+    }
+    
+    my $sess = $self->{'controller'}{'session'};
+    my $form = {
+        'action'        => 'useitem',
+        'pwd'           => $sess->pwdhash(),
+        'which'         => 3,
+        'whichitem'     => $self->{'id'},
+        'quantity'      => $count,
+    };
+    
+    my $resp = $sess->post('inv_use.php', $form);
+    $self->{'controller'}{'session'}->makeDirty();
+    return(undef) if (!$resp);
+    
+    my $results = {};
+    my $content = $resp->content();
+    while ($content =~ m/You (gain|acquire) ([\d,]+) (.+?)\./sg) {
+        my $val = $2;
+        my $thing = $3;
+        
+        $val =~ s/,//g;
+        $results->{$thing} += $val;
+    }
+    
+    return($results);
+}
+
 1;
